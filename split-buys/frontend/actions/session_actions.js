9b9456dd436170fb9f6b82831441dce2790b38e5
App Academy Student <email@email.com>
2022-04-22 17:26:10 -0400

    add session actions

---
+++ split-buys/frontend/actions/session_actions.js
@@ -0,0 +1,51 @@
+import { postUser, postSession, deleteSession } from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER,
+  };
+};
+
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const login = (formUser) => {
+  return (dispatch) => {
+    return postSession(formUser)
+      .then(user => dispatch(receiveCurrentUser(user)))
+      .fail(err => dispatch(receiveErrors(err)));
+  };
+};
+
+export const logout = () => dispatch => {
+  return deleteSession()
+    .then(() => dispatch(logoutCurrentUser()))
+    .fail((err) => dispatch(receiveErrors(err)));
+};
+
+export const signup = (formUser) => dispatch => {
+  return postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(err => dispatch(receiveErrors(err)));
+};
+
+
+
+
+
+
