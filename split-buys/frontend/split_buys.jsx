2db42efc104f8a70c0dd1cffb7e8755123a96d29
App Academy Student <email@email.com>
2022-04-22 18:04:09 -0400

    install redux-thunk and redux-logger

--- split-buys/frontend/split_buys.jsx
+++ split-buys/frontend/split_buys.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import Root from "./components/root";
+import { signup, login, logout } from './actions/session_actions';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;  
+
+  let store;
+  store = configureStore();
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to SplitBuys</h1>, root);
 });
+
