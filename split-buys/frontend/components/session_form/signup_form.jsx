e8d42a179b7981e5fca30deac006f76cab3b5f53
ds <swaby@protonmail.com>
2022-04-24 21:31:51 -0400

    style sign up page more and make it redirect on successful sign up

--- split-buys/frontend/components/session_form/signup_form.jsx
+++ split-buys/frontend/components/session_form/signup_form.jsx
@@ -16,7 +16,9 @@ class SignupForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state)
-        this.props.processForm(user);
+        this.props.processForm(user).then((res) => {
+            this.props.history.push('/test');
+        });
     }
 
     update(field) {
