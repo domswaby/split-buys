a80eec086f470e08350046ace1d5c73ee4b46130
App Academy Student <email@email.com>
2022-04-26 14:41:08 -0400

    finish styling left sidebar

--- split-buys/frontend/components/left_sidebar/left_sidebar.jsx
+++ split-buys/frontend/components/left_sidebar/left_sidebar.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { FaFlag } from 'react-icons/fa'; 
+import { GoListUnordered } from 'react-icons/go';
+import { GoPerson } from 'react-icons/go';
+import logo from './../../../app/assets/images/split-wise-logo.png'; 
 
 const LeftSidebar = () => {
   return (
     <div className="left-sidebar-wrap">
-      <h1>Left Sidebar</h1>
+      <div>
+        <div className="dashboard-tab">
+          <a href="#/dashboard"> <img src={logo} alt="" />Dashboard</a>
+        </div>
+        <div className="activity-tab">
+          <a href="#/activity"><FaFlag class="activity-flag" />Recent activity</a>
+        </div>
+        <div>
+          <div className="expenses-tab">
+            <a href="#/all"><GoListUnordered className="expenses-list-icon" />All expenses</a>
+          </div>
+          <div className="friends-list-wrap">
+            <h3> <span>FRIENDS</span> <span>+ add</span> </h3>
+            <ul>
+              <li>
+                  <GoPerson className="friend-icon"/>
+                <a href="">Friend 1</a>
+              </li>
+              <li>
+                  <GoPerson className="friend-icon"/>
+                <a href="">Friend 2</a>
+              </li>
+              <li>
+                <GoPerson className="friend-icon"/>
+                <a href="">Friend 3</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
