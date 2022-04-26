dad6da31e69a8e8960d41b1f0d235b01edd0aaac
ds <swaby@protonmail.com>
2022-04-25 23:46:03 -0400

    finish endorsements on homepage

--- split-buys/frontend/components/home/endorsements.jsx
+++ split-buys/frontend/components/home/endorsements.jsx
@@ -3,7 +3,32 @@ import React from "react";
 
 const Endorsements = () => {
     return (
-        <h1>Endorsements</h1>
+        <div className="endorsements-wrap">
+            <div>
+                <p>“Fundamental” for tracking finances. As good as WhatsApp for containing awkwardness.</p>
+                <p>Financial Times</p>
+            </div>
+            <div>
+                <p>Life hack for group trips. Amazing tool to use when traveling with friends! Makes life so easy!!</p>
+                <p>Ahah S, iOS</p>
+            </div>
+            <div>
+                <p>Makes it easy to split everything from your dinner bill to rent.</p>
+                <p>NY Times</p>
+            </div>
+            <div>
+                <p>So amazing to have this app manage balances and help keep money out of relationships. love it!</p>
+                <p>Haseena C, Android</p>
+            </div>
+            <div>
+                <p>I never fight with roommates over bills because of this genius expense-splitting app</p>
+                <p>Business Insider</p>
+            </div>
+            <div>
+                <p>I use it everyday. I use it for trips, roommates, loans. I love splitwise.</p>
+                <p>Trickseyus, iOS</p>
+            </div>
+        </div>
     )
 }
 
