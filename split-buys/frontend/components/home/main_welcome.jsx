94b7851e5d4a3341739b03a333615d86a49928af
ds <swaby@protonmail.com>
2022-04-25 21:05:34 -0400

    finish main welcome section of homepage

--- split-buys/frontend/components/home/main_welcome.jsx
+++ split-buys/frontend/components/home/main_welcome.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import airplane from './../../../app/assets/images/green_airplane.png'; // with import
+import Airplane from "../svgs/airplane";
+import House from "../svgs/house";
+import Heart from "../svgs/heart";
+import Star from "../svgs/star";
+import Apple from "../svgs/apple";
+import Android from "../svgs/android";
 
 const MainWelcome = () => {
    
     return (
         <div className="main-welcome-wrap">
-            <h1>Main Welcome</h1>
-            
+            <div>
+                <div className="main-info-wrap">
+                    <h1>Less stress when sharing expenses with <span>everyone</span></h1>
+                    <div class="welcome-icons">
+                        <Airplane /> 
+                        <House />
+                        <Heart /> 
+                        <Star /> 
+                    </div>
+                    <div className="main-welcome-description">
+                        <p>
+                            Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.
+                        </p>
+                    </div>
+                    <div class="big-signup-wrap">
+                        <button>Sign up</button>
+                    </div>
+                    <div className="main-welcome-icons">
+                        <p>Free for <Apple /> iPhone, <Android /> Android, and web.</p>
+                    </div>
+                </div>
+                <div className="main-picture-wrap">
+                    <img src={airplane} alt="green airplane" />
+                </div>
+            </div>
         </div>
     )
 }
