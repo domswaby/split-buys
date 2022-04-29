eb5fdb29db023445f99f983dcce7baeb5990232c
App Academy Student <email@email.com>
2022-04-29 17:19:35 -0400

    fix activity and expenses routes / containers

--- split-buys/frontend/components/activity/activity_container.jsx
+++ split-buys/frontend/components/activity/activity_container.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Activity from "./activity";
-import LeftSidebar from "../left_sidebar/left_sidebar";
+import LeftSidebarContainer from "../left_sidebar/left_sidebar_container";
 import RightSidebarAds from "../right_sidebar/right_sidebar_ads";
 
 const ActivityContainer = () => (
   <div className="app-container">
     <div>
-      <LeftSidebar />
+      <LeftSidebarContainer />
       <Activity />
       <RightSidebarAds />
     </div>
--- split-buys/frontend/components/expenses/expenses_container.jsx
+++ split-buys/frontend/components/expenses/expenses_container.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Expenses from "./expenses";
-import LeftSidebar from "../left_sidebar/left_sidebar";
 import RightSidebarAds from "../right_sidebar/right_sidebar_ads";
+import LeftSidebarContainer from "../left_sidebar/left_sidebar_container"; 
 
 const ExpensesContainer = () => (
   <div className="app-container">
     <div>
-      <LeftSidebar />
+      <LeftSidebarContainer />
       <Expenses />
       <RightSidebarAds />
     </div>
