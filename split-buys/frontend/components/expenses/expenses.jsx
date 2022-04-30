b2a4fefae66d06993ad1843032c6ab63c87fafbb
ds <swaby@protonmail.com>
2022-04-30 19:11:20 -0400

    connect create expense modal to expenses all page

--- split-buys/frontend/components/expenses/expenses.jsx
+++ split-buys/frontend/components/expenses/expenses.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import CreateExpenseModalContainer from "../modals/create_expense_modal_container";
 
 
-const Expenses = () => {
-  return (
-    <div className="dashboard-wrap">
-      <h1>All expenses
-        <div>
-          <button className="add-expense">Add an expense</button>
-          <button className="settle-button">Settle up</button>
+class Expenses extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+
+  }
+
+  toggleModal() {
+    let show = this.state.showModal;
+    this.setState({
+      showModal: !show
+    });
+  }
+
+  render(){
+      return (
+        <div className="dashboard-wrap">
+          <h1>All expenses
+            <div>
+              <button onClick={() => this.toggleModal()} className="add-expense">Add an expense</button>
+              <button className="settle-button">Settle up</button>
+            </div>
+          </h1>
+          <CreateExpenseModalContainer 
+            toggleModal={this.toggleModal}
+            showModal={this.state.showModal}
+          />
         </div>
-      </h1>
-    </div>
-  )
+      )
+  }
 }
 
 export default Expenses; 
