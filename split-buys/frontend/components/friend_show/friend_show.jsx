18f90b8b997c80bc63fe8fdb74487be0348c0969
ds <swaby@protonmail.com>
2022-04-30 19:19:05 -0400

    connect create expense modal to friend show page

--- split-buys/frontend/components/friend_show/friend_show.jsx
+++ split-buys/frontend/components/friend_show/friend_show.jsx
@@ -1,18 +1,40 @@
 import React from "react";
+import CreateExpenseModalContainer from "../modals/create_expense_modal_container";
 
-const FriendShow = (props) => {
-  
-  return (
-    <div className="dashboard-wrap">
-      <h1>
-        {props.friendInfo? (props.friendInfo.username.slice(0,1).toUpperCase() + props.friendInfo.username.slice(1).toLowerCase()) : null }
-        <div>
-          <button className="add-expense">Add an expense</button>
-          <button className="settle-button">Settle up</button>
-        </div>   
-      </h1>
-    </div>
-  )
+class FriendShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+
+  }
+
+  toggleModal() {
+    let show = this.state.showModal;
+    this.setState({
+      showModal: !show
+    });
+  }
+
+  render () {
+    return (
+      <div className="dashboard-wrap">
+        <h1>
+          {this.props.friendInfo? (this.props.friendInfo.username.slice(0,1).toUpperCase() + this.props.friendInfo.username.slice(1).toLowerCase()) : null }
+          <div>
+            <button onClick={() => this.toggleModal()} className="add-expense">Add an expense</button>
+            <button className="settle-button">Settle up</button>
+          </div>   
+        </h1>
+        <CreateExpenseModalContainer
+          toggleModal={this.toggleModal}
+          showModal={this.state.showModal}
+        />
+      </div>
+    )
+  }
 }
 
 export default FriendShow;
