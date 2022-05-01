1b595e51dfc394cdfac0e6ac8e154b0b2f2c6485
ds <swaby@protonmail.com>
2022-04-30 20:51:29 -0400

    connect friend modal to dropdown menu

--- split-buys/frontend/components/app_header/main_dropdown/main_dropdown.jsx
+++ split-buys/frontend/components/app_header/main_dropdown/main_dropdown.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import CreateExpenseModalContainer from "../../modals/create_expense_modal_container";
+import FriendModalContainer from "../../modals/friend_modal_container";
 
 class MainDropDown extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: false
+            showModal: false,
+            showFriendModal: false
         };
         this.toggleModal = this.toggleModal.bind(this);
+        this.toggleFriendModal = this.toggleFriendModal.bind(this);
 
     }
 
@@ -18,13 +21,20 @@ class MainDropDown extends React.Component {
         });
     }
 
+    toggleFriendModal() {
+        let show = this.state.showFriendModal;
+        this.setState({
+            showFriendModal: !show
+        });
+    }
+
     render () {
         return (
             <>
                 <div className="dropdown">
                     <button className="dropdown-toggle">{this.props.user.username}  <span>&#9660;</span></button>
                     <div className="dropdown-menu">
-                        <a href="#">Add Friend</a>
+                        <a onClick={() => this.toggleFriendModal()}>Add Friend</a>
                         <a onClick={() => this.toggleModal()}>Add Expense</a>
                         <a href="#" onClick={() => this.props.logout()}>Log Out</a>
                     </div>
@@ -32,6 +42,11 @@ class MainDropDown extends React.Component {
                         toggleModal={this.toggleModal}
                         showModal={this.state.showModal}
                     />
+                    <FriendModalContainer
+                        toggleFriendModal={this.toggleFriendModal}
+                        showFriendModal={this.state.showFriendModal}
+                    />
+
                 </div>
             </>
         )
