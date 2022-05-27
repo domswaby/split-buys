393701fa52054b0c9e74046951eb7552da0dd660
ds <swaby@protonmail.com>
2022-05-27 17:29:13 -0400

    fix spacing between about links

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const About = () => {
     return (
         <div id="links-and-help">
-            <a class="linked-in-link" href="https://www.linkedin.com" target="_blank"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png"/></a>
-            <a class="github-link" href="https://github.com/domswaby" target="_blank"><img src="https://img.icons8.com/nolan/64/github.png" /></a>            
+            <Link class="linked-in-link" href="https://www.linkedin.com" target="_blank"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png"/></Link>
+            <Link class="github-link" href="https://github.com/domswaby" target="_blank"><img src="https://img.icons8.com/nolan/64/github.png" /></Link>            
         </div>
     )
 }
